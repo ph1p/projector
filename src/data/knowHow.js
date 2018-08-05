dd45ceed3908540efb6ac91fb960becc555c4af3
ph1p <phil@capshake.com>
2018-08-05 21:01:55 +0200

    feat(knowhow): add urls and descriptions

--- src/data/knowHow.js
+++ src/data/knowHow.js
@@ -2,44 +2,44 @@ export default [
   {
     id: 1,
     name: 'react',
-    description: '',
-    url: ''
+    description: 'A JavaScript library for building user interfaces',
+    url: 'https://reactjs.org/'
   },
   {
     id: 2,
     name: 'vue',
-    description: '',
-    url: ''
+    description: 'The Progressive JavaScript Framework',
+    url: 'https://vuejs.org/'
   },
   {
     id: 3,
     name: 'angular',
-    description: '',
-    url: ''
+    description: 'One framework. Mobile & desktop.',
+    url: 'https://angular.io/'
   },
   {
     id: 4,
     name: 'sketch',
-    description: '',
-    url: ''
+    description: 'The digital design toolkit',
+    url: 'https://sketchapp.com/'
   },
   {
     id: 5,
     name: 'Adobe XD',
-    description: '',
-    url: ''
+    description: 'https://www.adobe.com/products/xd.html',
+    url: 'The future of experience design.'
   },
   {
     id: 6,
-    name: 'cinema4D',
+    name: 'Cinema 4D',
     description: '',
-    url: ''
+    url: 'https://www.maxon.net/en/products/cinema-4d/overview/'
   },
   {
     id: 7,
     name: 'AWS',
     description: '',
-    url: ''
+    url: 'https://aws.amazon.com/'
   },
   {
     id: 8,
@@ -50,20 +50,20 @@ export default [
   {
     id: 9,
     name: 'Node.js',
-    description: '',
-    url: ''
+    description: 'Node.js® is a JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
+    url: 'https://nodejs.org/'
   },
   {
     id: 10,
     name: 'Golang',
-    description: '',
-    url: ''
+    description: 'Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.',
+    url: 'https://golang.org/'
   },
   {
     id: 11,
     name: 'Rust',
-    description: '',
-    url: ''
+    description: 'Rust is a systems programming language that runs blazingly fast, prevents segfaults, and guarantees thread safety.',
+    url: 'https://www.rust-lang.org/'
   },
   {
     id: 12,
@@ -92,14 +92,14 @@ export default [
   {
     id: 16,
     name: 'Typescript',
-    description: '',
-    url: ''
+    description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.',
+    url: 'https://www.typescriptlang.org/'
   },
   {
     id: 17,
     name: 'Swift',
     description: '',
-    url: ''
+    url: 'https://swift.org/'
   },
   {
     id: 18,
@@ -110,8 +110,8 @@ export default [
   {
     id: 19,
     name: 'Kotlin',
-    description: '',
-    url: ''
+    description: 'Statically typed programming language for modern multiplatform applications',
+    url: 'https://kotlinlang.org/'
   },
   {
     id: 20,
@@ -123,163 +123,169 @@ export default [
     id: 21,
     name: 'Ruby',
     description: '',
-    url: ''
+    url: 'https://www.ruby-lang.org/'
   },
   {
     id: 22,
     name: 'Python',
     description: '',
-    url: ''
+    url: 'https://www.python.org/'
   },
   {
     id: 23,
     name: 'MongoDB',
     description: '',
-    url: ''
+    url: 'https://www.mongodb.com/'
   },
   {
     id: 24,
     name: 'Jira',
-    description: '',
-    url: ''
+    description: 'The #1 software development tool used by agile teams',
+    url: 'https://www.atlassian.com/software/jira'
   },
   {
     id: 25,
     name: 'Confluence',
-    description: '',
-    url: ''
+    description: 'Confluence is content collaboration software that changes how modern teams work',
+    url: 'https://www.atlassian.com/software/confluence'
   },
   {
     id: 26,
     name: 'Bamboo',
-    description: '',
-    url: ''
+    description: 'Continuous delivery, from code to deployment',
+    url: 'https://www.atlassian.com/software/bamboo'
   },
   {
     id: 27,
     name: 'Jenkins',
-    description: '',
-    url: ''
+    description: 'The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating any project.',
+    url: 'https://jenkins.io/'
   },
   {
     id: 28,
     name: 'Travic CI',
-    description: '',
-    url: ''
+    description: 'Easily sync your GitHub projects with Travis CI and you’ll be testing your code in minutes!',
+    url: 'https://travis-ci.org/'
   },
   {
     id: 29,
     name: 'git',
-    description: '',
-    url: ''
+    description: 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.',
+    url: 'https://git-scm.com/'
   },
   {
     id: 30,
     name: 'SASS',
-    description: '',
-    url: ''
+    description: 'Sass is the most mature, stable, and powerful professional grade CSS extension language in the world.',
+    url: 'https://sass-lang.com/'
   },
   {
     id: 31,
     name: 'LESS',
-    description: '',
-    url: ''
+    description: 'It\'s CSS, with just a little more.',
+    url: 'http://lesscss.org/'
   },
   {
     id: 32,
     name: 'BEM',
-    description: '',
-    url: ''
+    description: 'BEM — Block Element Modifier is a methodology that helps you to create reusable components and code sharing in front-end development',
+    url: 'http://getbem.com/'
   },
   {
     id: 33,
     name: 'Redux',
-    description: '',
-    url: ''
+    description: 'Redux is a predictable state container for JavaScript apps.',
+    url: 'https://redux.js.org/'
   },
   {
     id: 34,
     name: 'MobX',
-    description: '',
-    url: ''
+    description: 'Simple, scalable state management',
+    url: 'https://mobx.js.org/'
   },
   {
     id: 35,
     name: 'RxJava',
-    description: '',
-    url: ''
+    description: 'An API for asynchronous programming with observable streams',
+    url: 'http://reactivex.io/'
   },
   {
     id: 36,
     name: 'RxJs',
-    description: '',
-    url: ''
+    description: 'An API for asynchronous programming with observable streams',
+    url: 'http://reactivex.io/'
   },
   {
     id: 37,
-    name: 'npm/yarn',
-    description: '',
-    url: ''
+    name: 'npm',
+    description: 'npm is the package manager for JavaScript and the world’s largest software registry. Discover packages of reusable code — and assemble them in powerful new ways.',
+    url: 'https://www.npmjs.com/'
+  },
+  {
+    id: 37,
+    name: 'yarn',
+    description: 'https://yarnpkg.com//',
+    url: 'FAST, RELIABLE, AND SECURE DEPENDENCY MANAGEMENT.'
   },
   {
     id: 38,
     name: 'webpack',
     description: '',
-    url: ''
+    url: 'https://webpack.js.org/'
   },
   {
     id: 39,
     name: 'rollup',
     description: '',
-    url: ''
+    url: 'https://rollupjs.org/'
   },
   {
     id: 40,
     name: 'parcel',
-    description: '',
-    url: ''
+    description: 'Blazing fast, zero configuration web application bundler',
+    url: 'https://parceljs.org/'
   },
   {
     id: 41,
     name: 'nuxt.js',
-    description: '',
-    url: ''
+    description: 'Universal Vue.js Applications',
+    url: 'https://nuxtjs.org/'
   },
   {
     id: 42,
     name: 'next.js',
-    description: '',
-    url: ''
+    description: 'Next.js is a lightweight framework for static and server‑rendered applications.',
+    url: 'https://nextjs.org/'
   },
   {
     id: 43,
     name: 'flow',
-    description: '',
-    url: ''
+    description: 'FLOW IS A STATIC TYPE CHECKER FOR JAVASCRIPT',
+    url: 'https://flow.org/'
   },
   {
     id: 44,
     name: 'JWT',
-    description: '',
-    url: ''
+    description: 'JSON Web Tokens are an open, industry standard RFC 7519 method for representing claims securely between two parties.',
+    url: 'http://jwt.io/'
   },
   {
     id: 45,
     name: 'OAuth 2.0',
-    description: '',
-    url: ''
+    description: 'OAuth 2.0 is the industry-standard protocol for authorization. OAuth 2.0 supersedes the work done on the original OAuth protocol created in 2006.',
+    url: 'https://oauth.net/2/'
   },
   {
     id: 46,
     name: 'nginx',
     description: '',
-    url: ''
+    url: 'https://www.nginx.com/'
   },
   {
     id: 47,
     name: 'apache',
-    description: '',
-    url: ''
+    description: 'The Apache HTTP Server Project is an effort to develop and maintain an open-source HTTP server for modern operating systems including UNIX and Windows.',
+    url: 'https://httpd.apache.org/'
   },
   {
     id: 48,
@@ -296,7 +302,7 @@ export default [
   {
     id: 50,
     name: 'GraphQL',
-    description: '',
-    url: ''
+    description: 'GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data.',
+    url: 'https://graphql.org/'
   }
 ];
